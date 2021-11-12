6f6169da0a4046191f1aadb5ce8d55d7f554e03a
pmmorgia <pmmorgia@gmail.com>
2021-11-12 22:07:51 +0800

    clean

--- blendsite/oreblend/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OreblendConfig(AppConfig):
-    name = 'oreblend'
--- blendsite/oreblend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import HomeView
-
-
-app_name="oreblend"
-
-urlpatterns = [
-    path('home', HomeView.as_view(), name="home"),
-]
